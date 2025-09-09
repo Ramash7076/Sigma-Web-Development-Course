bc7a05f7f3323b012b0fc7e38c485bfee8d373ee
Ramash Halder <ramash7076@gmail.com>
2025-09-09 23:05:17 +0530

    129

---
+++ video 129 -- Layouts in Next.js/layouts/app/about/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore possimus expedita eius aut nihil voluptate. Impedit animi, quas dolor numquam nemo aperiam voluptas sed incidunt nisi. Obcaecati, dicta veritatis. Esse voluptate consequuntur repellat molestiae?
+    </div>
+  )
+}
+
+export default page
